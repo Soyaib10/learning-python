b2ae52553b311b591d70aa7a48cf47f3464e9ac2
Soyaib Zihad <93576102+Soyaib10@users.noreply.github.com>
2023-05-28 18:23:37 +0600

    Add files via upload

---
+++ Basics/3-Mad_Labs_Game.py
@@ -0,0 +1,7 @@
+name = input("Enter name: ")
+color = input("Enter a color: ")
+celebrity = input("Enter a celebrity: ")
+
+print("My name is: " + name)
+print("My favourite color: " + color)
+print("My favourite footballer: " + celebrity)
