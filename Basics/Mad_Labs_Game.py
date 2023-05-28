42d34a6e45892d01e4864848de1545b0bae32085
Soyaib Zihad <93576102+Soyaib10@users.noreply.github.com>
2023-05-28 18:22:41 +0600

    Delete Basics directory

--- Basics/Mad_Labs_Game.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter name: ")
-color = input("Enter a color: ")
-celebrity = input("Enter a celebrity: ")
-
-print("My name is: " + name)
-print("My favourite color: " + color)
-print("My favourite footballer: " + celebrity)
